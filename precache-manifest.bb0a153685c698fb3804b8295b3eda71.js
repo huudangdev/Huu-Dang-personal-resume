45624bc749500dcfe4de4dfd3283be1b451f3cba
huudangdev <nguyentranhuudang.blog@gmail.com>
2019-06-29 22:04:00 +0700

    Updates

---
+++ precache-manifest.bb0a153685c698fb3804b8295b3eda71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a66acf2fa2b9bdaf3f44a394d17142",
+    "url": "/Huu-Dang-personal-resume/index.html"
+  },
+  {
+    "revision": "f97ef9c91f9a7a590ac2",
+    "url": "/Huu-Dang-personal-resume/static/css/main.2b9e8329.chunk.css"
+  },
+  {
+    "revision": "e63506c30a05fa6ba4eb",
+    "url": "/Huu-Dang-personal-resume/static/js/2.54c69034.chunk.js"
+  },
+  {
+    "revision": "f97ef9c91f9a7a590ac2",
+    "url": "/Huu-Dang-personal-resume/static/js/main.a31888ef.chunk.js"
+  },
+  {
+    "revision": "43920206fff887c984e6",
+    "url": "/Huu-Dang-personal-resume/static/js/runtime~main.1aa94492.js"
+  }
+]);
